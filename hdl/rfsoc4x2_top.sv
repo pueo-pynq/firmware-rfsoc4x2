5793f19e64c1af2545d598dd35d4586451eae970
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-02-14 13:34:32 -0500

    add base HDLs

---
+++ hdl/rfsoc4x2_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/14/2024 01:34:03 PM
+// Design Name: 
+// Module Name: rfsoc4x2_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rfsoc4x2_top(
+
+    );
+endmodule
